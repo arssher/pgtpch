bb4667036b0633465b16bb2b8f0a91dac40ba6ca
Arseny Sher <arsv.sher@gmail.com>
2016-12-20 18:33:59 +0000

    Line numbers now printed on error

--- common.sh
+++ common.sh
@@ -1,3 +1,17 @@
+error() {
+  local parent_lineno="$1"
+  local message="$2"
+  local code="${3:-1}"
+  if [[ -n "$message" ]] ; then
+    echo "Error on or near line ${parent_lineno}: ${message}; exiting with status ${code}"
+  else
+    echo "Error on or near line ${parent_lineno}; exiting with status ${code}"
+  fi
+  exit "${code}"
+}
+# on error, print bad line number and exit
+trap 'error ${LINENO}' ERR
+
 LASTCONF="pgtpch_last.conf"
 CONFIGFILE="pgtpch.conf"
 
--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -e
 
 show_help() {
     cat <<EOF
